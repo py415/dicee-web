e977091b8c231005b81f831d4ddb1b2fb3076bc3
Philip Yu <pyu3@mail.sfsu.edu>
2019-09-12 19:54:38 -0700

    Update h1 tag with winner message

--- index.js
+++ index.js
@@ -12,3 +12,12 @@ var image2 = document.querySelectorAll("img")[1];
 
 image1.setAttribute("src", randomImageSource1);
 image2.setAttribute("src", randomImageSource2);
+
+// update h1 tag with winner message
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "Player 1 Wins!";
+} else if (randomNumber2 > randomNumber1) {
+  document.querySelector("h1").innerHTML = "Player 2 Wins!";
+} else if (randomNumber1 === randomNumber2) {
+  document.querySelector("h1").innerHTML = "Draw!";
+}
